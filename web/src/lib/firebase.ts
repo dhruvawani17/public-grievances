5e3eec4b8a84e55ec75fe84ff82e4e942d7b633e
dhruvawani17 <dhruvawani17@gmail.com>
2025-12-09 11:05:58 +0530

    done till 12pm

--- web/src/lib/firebase.ts
+++ web/src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCGYdRpuJ7sRjl3zDemdsc0tP1H2MPne-I",
-  authDomain: "local-grievances-fa36a.firebaseapp.com",
-  projectId: "local-grievances-fa36a",
-  storageBucket: "local-grievances-fa36a.firebasestorage.app",
-  messagingSenderId: "715104194406",
-  appId: "1:715104194406:web:ce5673e15d43171e3082a7"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
